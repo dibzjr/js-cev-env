e4258c4245860914104c94899e416df34a9ce2de
Rogers-Mudibo\HP <rmudibo@nanodigital.co.ke>
2019-07-30 23:11:45 +0300

    Module 9 work in progress

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,43 @@
+//  var express = require('express');
+ import express from 'express';
+
+// var path = require('path');
+import path from 'path';
+// var open = require('open');
+import open from 'open';
+
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+
+
+
+
+// var port = 3030;
+const port = 3030;
+
+// var app = express();
+const app = express();
+
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler,{
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'))
+});
+
+
+app.listen(port,(err)=>{
+   if(err){
+     console.log(err);//eslint-disable-line no-console
+
+   }else{
+     open('http://localhost:' + port);
+   }
+});
+
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+// let chalk = require('chalk');
+import chalk from 'chalk';
+
+
+console.log(chalk.green("Starting app in dev mode..."));//eslint-disable-line no-console
+
